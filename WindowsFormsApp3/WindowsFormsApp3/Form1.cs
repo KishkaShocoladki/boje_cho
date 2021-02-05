c43f492a211a3f69bf2674cd011964b61d4c7be0
Mike Milligram <55143644+KishkaShocoladki@users.noreply.github.com>
2021-02-05 20:31:53 +0400

    кхЪ

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        int widh;
+        public Form1(int with)
+        {
+            InitializeComponent();
+            widh = Width;
+            Text = (Width).ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        { 
+            if (Width > 200)
+            {
+               // MessageBox.Show("xnj");
+                e.Cancel = true;
+            }
+            Application.OpenForms[0].Close();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Form form1 = new Form1(widh);
+            form1.Width = Width * 20 / 100;
+            form1.ShowDialog();
+        }
+    }
+}
